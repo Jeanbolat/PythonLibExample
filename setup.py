db0fd2bf7d1eceeac2d7d05484cf41fed9a3a575
Jeanbolat <kagantegin@gmail.com>
2020-08-03 20:35:22 +0600

    text

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name='libarary1',
+    url='https://github.com/Jeanbolat/PythonLibExample',
+    author='Kagan',
+    packages=['library1'],
+    install_requires=[],
+    license="MIT",
+    description="Library for me",)
